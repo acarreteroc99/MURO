3cd4f11e2cde817ca9f7904204ac0cb8b54b9f2d
acarreteroc99 <acarreteroc99@gmail.com>
2022-09-04 18:09:06 +0200

    AD_Enumerator script is almost completed

--- src/AD Enumerator/AD_Enumerator.ps1
+++ src/AD Enumerator/AD_Enumerator.ps1
@@ -28,7 +28,7 @@ Param(
     [switch]$All,
     [switch]$Computers,
     [switch]$Domains,
-    [switch]$GPOs,
+    [switch]$Forest,
     [switch]$Groups, 
     [switch]$OUs,
     [switch]$Users
@@ -37,7 +37,7 @@ Param(
 If($All -eq $True){
     $Computers = $True
     $Domains = $True
-    $GPOs = $True
+    $Forest= $True
     $Groups = $True
     $OUs = $True
     $Users = $True 
@@ -46,6 +46,8 @@ If($All -eq $True){
 # Importing Modules
 Import-Module ActiveDirectory
 
+$global:JSON_PATH = "../../json"
+
 function extractDomain($str){
     $pos = $str.IndexOf('.') + 1
     $dom = (($str)).substring($pos)
@@ -53,39 +55,79 @@ function extractDomain($str){
     return $dom
 }
 
+function Get_AllDomains($rootDom){
+    $dom = @()
+    $tmp = Get-ADDomain -Identity "$rootDom" | Select ChildDomains
+
+    while($tmp -ne $null){
+        $dom += $tmp.ChildDomains
+
+        ForEach($aux in $tmp){
+            $aux2 = $aux.ChildDomains
+            If($aux -ne $null){
+                $tmp2 += Get-ADDomain -Identity "$aux2" | Select ChildDomains
+            }
+        }
+        $tmp = $tmp2.ChildDomains
+    }
+
+    return $dom
+}
+
 # Computer extraction
 If($Computers -eq $True){
     Get-ADComputer -Filter * -Properties * `
-    | Select -Property Name,DNSHostName,Enabled,LastLogonDate `
-    | ConvertTo-Json
+    | Select -Property Name,DNSHostName,Enabled,ObjectClass,OperatingSystem `
+    | ConvertTo-Json `
+    | Out-File "$JSON_PATH/computers.json"
 
-    # Call function extractDomain(str) to extract the domain from the properties
+    # Extract Domain and insert in Json
+    # To search in multiple domains, do: Get-ADComputer -Filter * -SearchBase "DC=MyOtherDomain,DC=com" -Server "MyOtherDomain.com" 
 }
-
-# To search in multiple domains, do: Get-ADComputer -Filter * -SearchBase "DC=MyOtherDomain,DC=com" -Server "MyOtherDomain.com" 
 
 # Domains extraction
-If($Domains -eq $True){
-    
+If($Domains -eq $True){ 
+    $all_doms = @($ADDomain)
+    $all_doms += Get_AllDomains($ADDomain)
+    ConvertTo-Json -InputObject $all_doms `
+    | Out-File "$JSON_PATH/domains.json"
 }
 
-# GPO extraction
-If($GPOs -eq $True){
-    
+
+# Forest extraction
+If($Forest -eq $True){
+    Get-ADForest `
+    | Select Domains,Name,RootDomain,Sites `
+    | ConvertTo-Json `
+    | Out-File "$JSON_PATH/forest.json"
 }
+
 
 # Groups extraction
 If($Groups -eq $True){
-    
+    Get-ADGroup -Filter * `
+    | Select Name,DistinguishedName,GroupScope `
+    | ConvertTo-Json `
+    | Out-File "$JSON_PATH/groups.json"
+
+    # Extract domain and insert in Json
 }
 
 # OUs extraction
 If($OUs -eq $True){
-    
+    Get-ADOrganizationalUnit -Filter * `
+    | Select City,Country,Name `
+    | ConvertTo-Json `
+    | Out-File "$JSON_PATH/OUs.json"
 }
 
 # Users extraction
 If($Users -eq $True){
-    
+    Get-ADUser -Filter * `
+    | Select Name,SAMAccoutnName,UserPrincipalName `
+    | ConvertTo-Json `
+    | Out-File "$JSON_PATH/users.json"
+
+    # To search in multiple domains, do: Get-ADUser -Filter * -SearchBase "DC=MyOtherDomain,DC=com" -Server "MyOtherDomain.com"
 }
 
