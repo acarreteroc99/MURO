c38df336de9829e9d05da7c6786cd5414a04f2e1
acarreteroc99 <acarreteroc99@gmail.com>
2022-11-01 23:56:54 +0100

    Problems with "Get-Date" PS utility when writing logs. PENDING TO BE SOLVED

--- src/fwEditor.ps1
+++ src/fwEditor.ps1
@@ -29,7 +29,8 @@ Param(
 
 $PROFTEMPL_PATH = "../json/Profiles/Templates";
 $PROFTARGT_PATH = "../json/Profiles/Targets";
-$MODULES_PATH = "./Modules"
+$MODULES_PATH = "./Modules";
+$LOGS_PATH = "../logs";
 
 . "$MODULES_PATH/ModuleProfiles.ps1"
 . "$MODULES_PATH/ModuleTargets.ps1"
@@ -73,12 +74,15 @@ function Filename-Extractor([array]$arrayInp,[string]$flagName){
 
 function Rule-Generator($webInp, $profFN, $targFN){
 
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] 'Rule-Generator' function has been entered" >> "$LOGS_PATH/fwEditor.log";
+
     $rules = @();
 
     # ============   WEBINPUT   ==============
     $webCommand = "Set-NetFirewallRule ";
 
-    # First, the "standard" defined by the user is created
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Creation of the rule defined by the user through the GUI has started" >> "$LOGS_PATH/fwEditor.log";
+
     for($pos=0; $pos -le $webInp.length-1; $pos+=2){
 
         Switch($webInp[$pos]){
@@ -89,22 +93,27 @@ function Rule-Generator($webInp, $profFN, $targFN){
         }
 
         if($webInp[$pos] -eq "dir"){
+            Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Rule's direction is being extracted" >> "$LOGS_PATH/fwEditor.log";
             $dir = $webInp[$pos+1];
         }
     }
 
-    # =============   TARGETS   ===============
-    # If targets have been defined, the rule will apply to all of them
+    # =============   TARGETS   ===============  
+    # If a 'target' file has been defined, its content extraction will get started
+
     if($targFN.Length -ne 0){
+        Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Calling ModuleTargets.ps1" >> "$LOGS_PATH/fwEditor.log";
         $targetsInfo = Implement-Targets $targFN;
         $targetList = @();
 
         if($dir -eq "inbound"){
+            Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Extracting inbound targets" >> "$LOGS_PATH/fwEditor.log";
             $numEl = $targetsInfo[0];
             $title = $targetsInfo[1];
             $recipients = $targetsInfo[2];
         }
         else {
+            Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Extracting outbound targets" >> "$LOGS_PATH/fwEditor.log";
             $numEl = $targetsInfo[3];
             $title = $targetsInfo[4];
             $recipients = $targetsInfo[5];
@@ -113,6 +122,7 @@ function Rule-Generator($webInp, $profFN, $targFN){
         $ctr=0;
         $acc=0;
 
+        Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Flags are being added to the targets according to the specified group" >> "$LOGS_PATH/fwEditor.log";
         foreach($el in $title){
             for($i=0; $i -le $numEl[$ctr]; $i++){
                 Switch($el){
@@ -128,7 +138,7 @@ function Rule-Generator($webInp, $profFN, $targFN){
         }
     }
 
-    # Merging rule coming from web with targets
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Rule coming from the website/GUI is being merged with specified targets" >> "$LOGS_PATH/fwEditor.log";
     foreach($el in $targetList){
         $rules += $webCommand + $el + ";";
     }
@@ -136,9 +146,12 @@ function Rule-Generator($webInp, $profFN, $targFN){
     # ============   PROFILES   ================
     # If a profile has been defined, then rules will be "concatenated" to the one defined by the user
     if($profFN.Length -ne 0){
+        Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Calling ModuleProfiles.ps1" >> "$LOGS_PATH/fwEditor.log";
+
         $profileRules = Implement-Profiles $profFN;
         $listOfProfRules = @();
 
+        Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Creating rule from profiles' contents" >> "$LOGS_PATH/fwEditor.log";
         foreach($aux in $profileRules){
             foreach($rule in $aux){
                 $aux2 = "Set-NetFirewallRule " + $rule;
@@ -147,11 +160,16 @@ function Rule-Generator($webInp, $profFN, $targFN){
         }
     }
 
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Merging rules created from profiles with targets extracted previously" >> "$LOGS_PATH/fwEditor.log";
+
     foreach($target in $targetList){
         foreach($profRule in $listOfProfRules){
             $rules += $profRule + $target + ";";
         }
     }
+
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Exiting 'Rule-Generator' function" >> "$LOGS_PATH/fwEditor.log";
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Results are being returned to caller... " >> "$LOGS_PATH/fwEditor.log";
 
     return $rules;
 }
@@ -159,6 +177,7 @@ function Rule-Generator($webInp, $profFN, $targFN){
 function main{
 
     if($WebInput.Length -gt 0){
+        Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Calling 'WebInput-Parser' function" >> "$LOGS_PATH/fwEditor.log";
         $webInp = WebInput-Parser $WebInput;
     }
     <#
@@ -170,27 +189,32 @@ function main{
     }
     #>
 
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Extracting filenames from Profiles and Targets files (if specified)" >> "$LOGS_PATH/fwEditor.log";
     $profFN = Filename-Extractor $webInp "Profiles"
     $targFN = Filename-Extractor $webInp "Targets";
 
+    Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Calling 'Rule-Generator' function..." >> "$LOGS_PATH/fwEditor.log";
     $new_rules = Rule-Generator $webInp $profFN $targFN;
 
     Write-Output $new_rules;
 }
 
-# 'main' function is invoked. Create to keep the script clean. 
+# 'main' function is invoked. Create to keep the script clean.
+Write-Output "[$((Get-Date -Format d).ToString()) $((Get-Date -Format t).ToString())] Calling function 'main" >> "$LOGS_PATH/fwEditor.log"; 
 main;
 
 <# NEXT STEPS   
 
-2. "Rule-Generator" function must be finished. All information gathered from Profile, Targets and User's input must be combined to create a set of rules - COMPLETED
-that will be executed in the remote computers by using the "Invoke-Expression" command from powershell. 
+2. "Rule-Generator" function must be finished. All information gathered from Profile, Targets and User's input must be combined to create a set of rules that will be executed in the remote computers by using the "Invoke-Expression" command from powershell - COMPLETED
+
 3. The main "flow" of the program shall be reviewed (main function)
-4. Integrate the "fwEditor.ps1" with the "AD_Enumerator.ps1" to finally create the tool
-    4.1 NOTE: It makes more sense to integrate the AD_Enumerator.ps1 with the GUI, and then the GUI with the "FWEditor.ps1"
+
 5. Maybe, transform the tool into a more modular tool to make it easier for people to do extra things. 
     5.1 This means to separate each feature into different files, and then have a sole, main file that calls functions from each
     file. 
+
+6. Comment code
+7. Create logs for each script
 #>
 
 
